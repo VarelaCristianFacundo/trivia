57bbd4ef658bb0ed12cbd17290753a20c0c6eab5
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2016-09-03 17:19:15 -0300

    Inicio de Trivia
    Se coloca signo de pregunta y se asocia a la base de datos de trivia

---
+++ www/js/controllers.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+  $('#saludar').on('click', function(){
+    alert ("hola mundo");
+  })
+})
+
+.controller('ChatsCtrl', function($scope, Chats, $timeout) {
+ 
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+ $scope.preguntas= [];
+  var pregRef = new Firebase('https://tp1trivia.firebaseio.com/preguntas');
+
+    pregRef.on('child_added', function (snapshot) {
+    $timeout(function(){
+
+    var preg = snapshot.val();
+   // console.log(preg);
+
+    $scope.preguntas.push(preg);
+    console.log($scope.preguntas);  
+    });
+   });
+
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, $timeout, Chats) {
+  $('#enviar').on('click', function(){
+    alert ($stateParams.chatId);
+  })
+  $scope.chat = Chats.get($stateParams.chatId);
+alert ($stateParams.chatId);
+  $scope.preguntas= [];
+  var pregRef = new Firebase('https://tp1trivia.firebaseio.com/preguntas');
+
+    pregRef.on('child_added', function (snapshot) {
+    $timeout(function(){
+
+    var preg = snapshot.val();
+   // console.log(preg);
+
+    $scope.preguntas.push(preg);
+    console.log($scope.preguntas);  
+    });
+   });
+
+
+
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
