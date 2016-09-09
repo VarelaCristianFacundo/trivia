9aa7dd12eccdad8d3c3f977ae5f34bf0fedc1bf6
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2016-09-08 23:07:31 -0300

    Modificación de íconos de APP

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('starter.controllers', [])
 
     var user = snapshot.val();
     $scope.usuario.push(user);
-    console.log(user.u1);
+//    console.log($scope.usuario);
 
     });
    });
